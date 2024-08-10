20e905308a1d31d08181a81e1daf50cef3cf02d3
David Stevenson <drstevenson1958@yahoo.com>
2024-08-10 17:20:39 -0400

    Understanding JavaScript Types

--- ch02/UnderstandingJavaScriptTypes.js
+++ ch02/UnderstandingJavaScriptTypes.js
@@ -4,3 +4,11 @@ let score = 9;
 console.log("score", score, typeof score);
 let date = new Date(2022, 10, 1);
 console.log("date", date, typeof date);
+score = "ten" ;
+console.log("score", score, typeof score);
+
+// Output
+// firstName Fred string
+// score 9 number
+// date 2022-11-01T04:00:00.000Z object
+// score ten string
