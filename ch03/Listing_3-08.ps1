86cc91c7583fedcfe502f5ddcedcb22f4c991a29
David Stevenson <drstevenson1958@yahoo.com>
2024-08-11 17:00:17 -0400

    Adding Babel
    npm install --save-dev @babel/core
    npm i -D @babel/preset-env
    npm i -D @babel/preset-react
    npm i -D @babel/preset-typescript
    npm i -D @babel/plugin-transform-runtime @babel/runtime

--- ch03/Listing_3-08.ps1
+++ ch03/Listing_3-08.ps1
@@ -1,3 +1,7 @@
 # Adding Babel
 
 npm install --save-dev @babel/core
+npm i -D @babel/preset-env
+npm i -D @babel/preset-react
+npm i -D @babel/preset-typescript
+npm i -D @babel/plugin-transform-runtime @babel/runtime
