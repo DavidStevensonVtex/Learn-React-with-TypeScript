e3ee99f800086889a9fd05e64b769796b8d56e9b
David Stevenson <drstevenson1958@yahoo.com>
2024-08-11 14:30:51 -0400

    TypeScript - Creating Union Types

---
+++ ch02/CreatingUnionTypes.ts
@@ -0,0 +1,8 @@
+type Level = "H" | "M" | "L";
+type RGB = "red" | "green" | "blue";
+
+let color: RGB = "red";
+
+// Errors in code
+// Type '"yellow"' is not assignable to type 'RGB'.
+// color = "yellow" ;
