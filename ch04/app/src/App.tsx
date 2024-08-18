9eea0669a8db59b871075b17c7096f15647fe00e
David Stevenson <drstevenson1958@yahoo.com>
2024-08-18 14:22:41 -0400

    Listing 4-8 - Modify App.tsx to add PersonScore component.
    Remove Prettier / ESLint plug in because it was becoming obnoxious

--- ch04/app/src/App.tsx
+++ ch04/app/src/App.tsx
@@ -1,8 +1,13 @@
+// Listing 4-8 - Modify App.tsx to add PersonScore component.
+
 import React from 'react';
 import './App.css';
+import { PersonScore } from './PersonScore';
 
 function App() {
-    return <div className="App"></div>;
+    return <div className="App">
+        <PersonScore />
+    </div>;
 }
 
 export default App;
