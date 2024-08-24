6b52958fb2f07e6ea701527e407f8f040c8ba8ab
David Stevenson <drstevenson1958@yahoo.com>
2024-08-24 16:18:20 -0400

    Listing 5-27 Create a production build by executing npm run build
    npm run build

---
+++ ch05/Listing_5-27.ps1
@@ -0,0 +1,8 @@
+# Listing 5-27 Create a production build by executing npm run build
+
+npm run build
+
+# Look at the build/static/css folder. Notice the Tailwind styles at the start of the file
+
+# An important point is that Tailwind does'nt add all its CSS classes - that would 
+# produce a masive CSS file! Instead, it only adds the CSS classes used in the app.
