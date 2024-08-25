aa1b8f896e6d319ec5b320d2a4bef6e77bd6585f
David Stevenson <drstevenson1958@yahoo.com>
2024-08-25 19:56:33 -0400

    Listing 6.24 Creating the HomePage component in the src/pages/HomePage.tsx file.

---
+++ ch06/my-app/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+// Listing 6.24 Creating the HomePage component in the src/pages/HomePage.tsx file.
+
+export function HomePage() {
+    return (
+        <div className="text-center p-5 text-xl">
+            <h1 className="text-xl text-slate-900">
+                Welcome to React Tools!
+            </h1>
+        </div>
+    )
+}
