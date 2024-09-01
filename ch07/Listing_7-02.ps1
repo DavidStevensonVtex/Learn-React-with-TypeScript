e1edc3bda3dc4094d8c8a7e0f86b0f1a95a3a4a0
David Stevenson <drstevenson1958@yahoo.com>
2024-09-01 15:10:00 -0400

    Listing 7-2 Installing and configuring Tailwind CSS
    Listing 7.3 Configuration changes to tailwind.config.js
    
    Install postcss library
    
    Generate configuration files for Tailwind and PostCSS

---
+++ ch07/Listing_7-02.ps1
@@ -0,0 +1,15 @@
+# Listing 7-2 Installing and configuring Tailwind CSS
+
+npm i -D tailwindcss
+
+# Generate configuration files for Tailwind and PostCSS
+
+npx tailwindcss init -p
+
+# Install postcss library
+
+npm i -D postcss
+
+# Generate configuration files for Tailwind and PostCSS
+
+npx tailwindcss init -p
