f0e7f05aea207ffadfc9c90c2e886125233d43a9
David Stevenson <drstevenson1958@yahoo.com>
2024-09-02 10:58:14 -0400

    Listing 8.10 Create src/Content.tsx file.

---
+++ ch08/statemanagement/src/Content.tsx
@@ -0,0 +1,21 @@
+// Listing 8.10 Create src/Content.tsx file.
+
+type Props = {
+    permissions: undefined | string[];
+}
+
+export function Content({ permissions }: Props) {
+    if (permissions === undefined) {
+        return null;
+    }
+
+    return permissions.includes("admin") ? (
+        <p className="mt-4 text-l text-center">
+            Some important stuff that only an admin can do
+        </p>
+    ) : (
+        <p className="mt-4 text-l text-center">
+            Insufficient permissions
+        </p>
+    )
+}
