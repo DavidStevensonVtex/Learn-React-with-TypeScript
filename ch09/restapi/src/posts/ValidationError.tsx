31a1cd95fb14634dbbd4da69020b64948925ee9e
David Stevenson <drstevenson1958@yahoo.com>
2024-09-07 17:31:14 -0400

    Listing 9.19 Add src/posts/ValidationError.tsx file.

---
+++ ch09/restapi/src/posts/ValidationError.tsx
@@ -0,0 +1,19 @@
+// Listing 9.19 Add src/posts/ValidationError.tsx file.
+
+import { FieldError } from "react-hook-form";
+
+type Props = {
+    fieldError: FieldError | undefined,
+}
+
+export function ValidationError({ fieldError }: Props) {
+    if (!fieldError) {
+        return null;
+    }
+
+    return (
+        <div role="alert" className="text-red-500 text-xs mt-1">
+            {fieldError.message}
+        </div>
+    );
+}
