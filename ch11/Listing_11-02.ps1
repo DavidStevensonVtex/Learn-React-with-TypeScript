dc2b89a64fef7b21e7f185ec82b18e5aef41a415
David Stevenson <drstevenson1958@yahoo.com>
2024-09-22 14:26:43 -0400

    Listing 11.2 Install Tailwind CSS, PostCSS, Autoprefixer, and initialize configuration files for Tailwind and PostCSS

---
+++ ch11/Listing_11-02.ps1
@@ -0,0 +1,13 @@
+# Listing 11.2 Install Tailwind CSS, PostCSS, Autoprefixer, and initialize configuration files for Tailwind and PostCSS
+
+# Installing and configuring Tailwind CSS
+npm i -D tailwindcss
+
+# Install postcss library
+npm i -D postcss
+
+# Install Autoprefixer, which adds vendor prefixes to CSS
+npm i -D autoprefixer
+
+# Generate configuration files for Tailwind and PostCSS
+npx tailwindcss init -p
