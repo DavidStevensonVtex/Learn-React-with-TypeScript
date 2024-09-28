661d816db205a785954932a154d5fe4eb77ec57e
David Stevenson <drstevenson1958@yahoo.com>
2024-09-28 16:59:23 -0400

    Listing 12.4 Invoke testing
    npm test

---
+++ ch12/Listing_12-04.ps1
@@ -0,0 +1,17 @@
+# Listing 12.4 Invoke testing
+
+npm test
+
+# Results
+# PASS  src/Checklist/assertValueCanBeRendered.test.ts
+# PASS  src/Checklist/assertValueCanBeRendered.test.ts
+# PASS  src/Checklist/assertValueCanBeRendered.test.ts
+# PASS  src/Checklist/assertValueCanBeRendered.test.ts
+# PASS  src/Checklist/isChecked.test.ts
+# PASS  src/Checklist/assertValueCanBeRendered.test.ts
+
+# Test Suites: 2 passed, 2 total
+# Tests:       3 passed, 3 total
+# Snapshots:   0 total
+# Time:        2.868 s, estimated 3 s
+# Ran all test suites.
