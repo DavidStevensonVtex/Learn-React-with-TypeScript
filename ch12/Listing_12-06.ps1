5c2ac16e21a1d6267806202b57673f18790d6f2b
David Stevenson <drstevenson1958@yahoo.com>
2024-09-28 17:05:13 -0400

    Listing 12.6 Running tests

---
+++ ch12/Listing_12-06.ps1
@@ -0,0 +1,13 @@
+# Listing 12.6 Running tests
+
+npm run test
+
+# Results
+# PASS  src/Checklist/isChecked.test.ts
+# PASS  src/Checklist/assertValueCanBeRendered.test.ts
+
+# Test Suites: 2 passed, 2 total
+# Tests:       4 passed, 4 total
+# Snapshots:   0 total
+# Time:        2.703 s
+# Ran all test suites.
