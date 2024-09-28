83ced0dba0702aa397b53fc7d36b0cf6de5a89c4
David Stevenson <drstevenson1958@yahoo.com>
2024-09-28 18:06:51 -0400

    Listing 12.10 Running the component test.

---
+++ ch12/Listing_12-10.ps1
@@ -0,0 +1,17 @@
+# Listing 12.10 Running the component test.
+
+npm test
+
+# Results
+# PASS  src/Checklist/Checklist.test.tsx
+# √ should render correct list items when data specified (59 ms)
+# √ should render correct list items when renderItem specified (5 ms)
+# PASS  src/Checklist/assertValueCanBeRendered.test.ts
+# PASS  src/Checklist/isChecked.test.ts  
+# PASS  src/Checklist/Checklist.test.tsx
+
+# Test Suites: 3 passed, 3 total
+# Tests:       7 passed, 7 total
+# Snapshots:   0 total
+# Time:        3.115 s
+# Ran all test suites.
