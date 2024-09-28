cf6ade670bdfffebaea4617f335d322a537eb054
David Stevenson <drstevenson1958@yahoo.com>
2024-09-28 18:18:48 -0400

    Listing 12.12 Run new test for checked items.

---
+++ ch12/Listing_12-12.ps1
@@ -0,0 +1,15 @@
+# Listing 12.12 Run new test for checked items.
+
+npm test
+
+# Results
+# PASS  src/Checklist/Checklist.test.tsx
+# √ should render correct list items when data specified (43 ms)
+# √ should render correct list items when renderItem specified (5 ms)
+# PASS  src/Checklist/Checklist.test.tsx
+
+# Test Suites: 2 skipped, 1 passed, 1 of 3 total
+# Tests:       7 skipped, 1 passed, 8 total
+# Snapshots:   0 total
+# Time:        3.094 s
+# Ran all test suites with tests matching "should render correct checked items when specified".
