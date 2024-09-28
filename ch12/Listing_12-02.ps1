249d66397b06cfcfe63a30ba12c06d43ac7ea482
David Stevenson <drstevenson1958@yahoo.com>
2024-09-28 16:47:34 -0400

    # Listing 12.2 Invoke testing
    npm test

---
+++ ch12/Listing_12-02.ps1
@@ -0,0 +1,14 @@
+# Listing 12.2 Invoke testing
+
+npm test
+
+# Results
+# PASS  src/Checklist/isChecked.test.ts
+# √ should be true when in checkedIds (2 ms)
+# √ should return false when not in checkedIds
+
+# Test Suites: 1 passed, 1 total
+# Tests:       2 passed, 2 total
+# Snapshots:   0 total
+# Time:        2.298 s
+# Ran all test suites.
